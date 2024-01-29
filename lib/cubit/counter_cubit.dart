6a7bec0587cd4fe0bd6e65a5ab26646b8c25c428
Aiden <97030039+AidenDexter@users.noreply.github.com>
2024-01-29 17:08:45 +0400

    feat: add tests

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,9 +1,10 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterState(counterValue: 0, wasIncremented: true));
+  CounterCubit() : super(const CounterState(counterValue: 0, wasIncremented: true));
 
   void increment() => emit(CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
 
--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,8 +1,11 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   final int counterValue;
   final bool wasIncremented;
 
-  CounterState({required this.counterValue, required this.wasIncremented});
+  const CounterState({required this.counterValue, required this.wasIncremented});
+
+  @override
+  List<Object> get props => [counterValue, wasIncremented];
 }
